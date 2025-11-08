3b790ad2c81db3ec56ea136b7fe0d6f9955c4b8d
Leon <leon@rexy.tech>
2025-11-08 17:16:53 +0700

    fix: student management entity

--- src/student-management/entities/student-management.entity.ts
+++ src/student-management/entities/student-management.entity.ts
@@ -1,11 +1,14 @@
 import { Staff } from "src/staff/entities/staff.entity";
-import { Entity, JoinColumn, OneToOne, ManyToOne } from "typeorm";
+import { Entity, JoinColumn, OneToOne, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Branch } from "src/branch/entities/branch.entity";
 
 
 @Entity('student_managements')
 
 export class StudentManagement {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
     @OneToOne(() => Staff, (staff) => staff.studentManagement)
     @JoinColumn({ name: 'staffId' })
     staff: Staff;
--- src/student-management/student-management.module.ts
+++ src/student-management/student-management.module.ts
@@ -3,9 +3,10 @@ import { StudentManagementService } from './student-management.service';
 import { StudentManagementController } from './student-management.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
+import { StudentManagement } from './entities/student-management.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, StudentManagement])],
   controllers: [StudentManagementController],
   providers: [StudentManagementService],
 })
